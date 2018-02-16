35e1feb8fea29adb3f01669fa1458fb9cc9c265a
Josh Mallett <playgroundinfinity@gmail.com>
2018-02-15 18:03:39 -0600

    Add files via upload

---
+++ Strings and Arrays/stringIsPermutation/isPermutation.py
@@ -0,0 +1,18 @@
+def main():
+    print('\n' + 'playground : lpaygruodn -> ' + str(isPermutation('playground', 'lpaygruodn')) + '\n')
+    print('hello : hello -> ' + str(isPermutation('hello', 'hello')) + '\n')
+    print('world : dlrow -> ' + str(isPermutation('world', 'dlrow')) + '\n')
+    print('123 : 1234 -> ' + str(isPermutation('123', '1234')) + '\n')
+
+def isPermutation(string1, string2):
+    if len(string1) == len(string2):
+        matchCount = 0
+        for c in string1:
+            if c in string2:
+                matchCount += 1
+        if matchCount == len(string1):
+            return True
+    return False
+
+if (__name__ == '__main__'):
+  main()
